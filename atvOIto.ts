737f071696ca80f2f90932dac409c7f74ea6da6d
Guilherme Campos <gh562061@gmail.com>
2025-06-17 15:41:35 -0300

    comit final

--- atvOIto.ts
+++ atvOIto.ts
@@ -2,3 +2,34 @@
 Em seguida, exiba a matriz na tela e encontre o maior
 elemento da matriz e a posição em que ele se encontra.*/
 
+
+console.clear();
+let teclado = require(`Prompt-sync`)();
+
+let minhaMatriz: number[][] = [];
+let numeroMaior: number = 0;
+let linha: number = 0;
+let coluna: number = 0;
+
+
+
+
+for (let z = 0; z < 3; z++) {
+    minhaMatriz[z] = [];
+    for (let x = 0; x < 3; x++) {
+
+        let numero: number = Math.floor(Math.random() * 21);
+
+        minhaMatriz[z][x] = numero;
+
+        if (numero > numeroMaior) {
+            numeroMaior = numero;
+            linha = z;
+            coluna = x;
+            
+        }
+    }
+
+}
+console.log(`O maior número da matriz é ${numeroMaior} e está na linha ${linha} e coluna ${coluna}`);
+console.log(minhaMatriz);
